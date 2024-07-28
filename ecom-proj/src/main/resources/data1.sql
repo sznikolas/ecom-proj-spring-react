3939dc7bcc2a0c4224eedbad0852783ab183d44b
sznikolas <szocs.nikolas@gmail.com>
2024-07-28 11:58:37 +0200

    first commit

---
+++ ecom-proj/src/main/resources/data1.sql
@@ -0,0 +1,4 @@
+INSERT INTO product (name, description, brand, price, category, release_date, available, quantity) VALUES
+                  ('Tata Nexon', 'A compact SUV', 'Tata Motors', 25000.00, 'Cars', '2024-07-12', true, 100),
+                  ('Maruti Suzuki Swift', 'Blabla', 'Maruti Suzuki', 20000.00, 'Cars', '2024-07-11', true, 55),
+                  ('Hyundai Creta', 'A modern family car', 'Hyundai', 30000.00, 'Cars', '2024-07-10', true, 74);
