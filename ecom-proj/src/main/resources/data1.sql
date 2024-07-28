cc7e794095a851b3f42e7206040117069c3e399d
sznikolas <szocs.nikolas@gmail.com>
2024-07-28 12:15:52 +0200

    changed properties name

--- ecom-proj/src/main/resources/data1.sql
+++ ecom-proj/src/main/resources/data1.sql
@@ -1,4 +1,4 @@
-INSERT INTO product (name, description, brand, price, category, release_date, available, quantity) VALUES
+INSERT INTO product (name, description, brand, price, category, release_date, product_available, stock_quantity) VALUES
                   ('Tata Nexon', 'A compact SUV', 'Tata Motors', 25000.00, 'Cars', '2024-07-12', true, 100),
                   ('Maruti Suzuki Swift', 'Blabla', 'Maruti Suzuki', 20000.00, 'Cars', '2024-07-11', true, 55),
                   ('Hyundai Creta', 'A modern family car', 'Hyundai', 30000.00, 'Cars', '2024-07-10', true, 74);
